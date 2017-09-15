8fce623f12d666745283fa8d1c4dc3111263b623
Tyler Pawlaczyk <pawlactb@clarkson.edu>
2017-09-15 19:30:00 +0500

    update

--- src/main/ClypeClient.java
+++ src/main/ClypeClient.java
@@ -10,6 +10,7 @@ public class ClypeClient {
 	
 	private static final int    DEFAULT_PORT = 7000;
 	private static final String DEFAULT_HOST = "localhost";
+	private static final String DEFAULT_USER = "Anon";
 	
 	//members
 	private String    userName, hostName;
@@ -43,10 +44,76 @@ public class ClypeClient {
 	/**
 	 * @param userName Username of client.
 	 */
-	public ClypeClint(String userName) {
+	public ClypeClient(String userName) {
 		this(userName, DEFAULT_HOST, DEFAULT_PORT);
 	}
 	
+	public ClypeClient() {
+		this(DEFAULT_USER, DEFAULT_HOST, DEFAULT_PORT);
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+
+	public String getHostName() {
+		return hostName;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void start() {}
+	
+	public void sendData() {}
+	
+	public void receiveData() {}
+	
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (closeConnection ? 1231 : 1237);
+		result = prime
+				* result
+				+ ((dataToRecieveFromServer == null) ? 0
+						: dataToRecieveFromServer.hashCode());
+		result = prime
+				* result
+				+ ((dataToSendToServer == null) ? 0 : dataToSendToServer
+						.hashCode());
+		result = prime * result
+				+ ((hostName == null) ? 0 : hostName.hashCode());
+		result = prime * result + port;
+		result = prime * result
+				+ ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+
+	public boolean equals(Object other) {
+		boolean userName, hostName, port, connection, data;
+		
+		if(other instanceof ClypeClient)
+		{
+			other = ((ClypeClient)other);
+		}
+		else return false;
+		
+		userName = this.userName == other.userName;
+		hostName = this.hostName == other.hostName;
+		port = this.port == other.port;
+		connection = this.closeConnection == other.closeConnection;
+		
+		this.dataToSendToServer = null;
+		this.dataToRecieveFromServer = null;
+	}
+	
+	public String toString() {
+		return "ClypeClient\n User" + this.userName + "\n" +
+				"host: " + this.hostName + "\n" +
+				"port: " + this.port + "\n" +
+				this.dataToSendToServer + this.dataToRecieveFromServer;
+	}
 	
 
 }
