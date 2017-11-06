f9ed4e27dac5665a0ffcd908d1b5fb0cb21c436c
Tyler Pawlaczyk <pawlactb@clarkson.edu>
2017-11-05 20:08:06 -0500

    update

--- src/main/ClypeClient.java
+++ src/main/ClypeClient.java
@@ -4,7 +4,11 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.BindException;
+import java.net.ConnectException;
+import java.net.NoRouteToHostException;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
@@ -18,7 +22,8 @@ import data.MessageClypeData;
  */
 public class ClypeClient {
 	
-	private static final int    DEFAULT_PORT = 7000;
+	private static final int    DEFAULT_PORT =  7000;
+	private static final int    MIN_PORT     =  1024;
 	private static final String DEFAULT_HOST = "localhost";
 	private static final String DEFAULT_USER = "Anon";
 	
@@ -27,7 +32,7 @@ public class ClypeClient {
 	//members
 	private String    userName, hostName;
 	private int       port;
-	private boolean   closeConnection;
+	private boolean   closedConnection;
 	private ClypeData dataToSendToServer, dataToReceiveFromServer;
 	
 	private Scanner inFromStd = null;
@@ -46,12 +51,16 @@ public class ClypeClient {
 		this.hostName = hostName;
 		this.port = port;
 		
-		if(userName == null || hostName == null || port < 1024)
+		if(userName == null || hostName == null)
 		{
-			throw new IllegalArgumentException();
+			throw new NullPointerException("Host or Username is null");
 		}
 		
-		this.closeConnection = false;
+		if(port < this.MIN_PORT) {
+			throw new IllegalArgumentException("Port below 1024");
+		}
+		
+		this.closedConnection = false;
 		
 		this.dataToSendToServer = null;
 		this.dataToReceiveFromServer = null;
@@ -91,53 +100,79 @@ public class ClypeClient {
 		return port;
 	}
 	
-	public void start() {
-		inFromStd = new Scanner(System.in);
-		
-		try {
-			this.readClientData();
-		}
-		catch (IllegalArgumentException e) {
-			System.err.println("Illegal Argument!");
-			e.printStackTrace(System.err);
-		}
-		catch (IOException e) {
-			System.err.println("File error!");
-			e.printStackTrace(System.err);
-		}
-		
-		Socket sock;
-		
-		try {
-			sock = new Socket(hostName, port);
-			this.inFromServer = new ObjectInputStream(sock.getInputStream());
-			this.outToServer = new ObjectOutputStream(sock.getOutputStream());
-		}
-		catch (UnknownHostException e) {
-			System.err.println("Unknown host!");
-			e.printStackTrace();
-		}
-		catch (IOException e) {
-			System.err.println("IO error!");
-			e.printStackTrace(System.err);
-		}
-		
-		
-		
-		this.printData();
+	private void sendUserName() {
+		this.dataToSendToServer = new MessageClypeData(userName, userName, ClypeData.MESSAGE);
+		sendData();
 	}
+	
+	public void start() {
+		try {
+			this.inFromStd = new java.util.Scanner(System.in);
+			Socket socket = new Socket(this.hostName, this.port);
+
+			this.outToServer = new ObjectOutputStream(socket.getOutputStream());
+			this.inFromServer = new ObjectInputStream(socket.getInputStream());
+			System.out.println("Connected to server.");
+
+			Thread listener = new Thread(new ClientSideServerListener(this));
+			listener.start();
+
+			sendUserName();
+			System.out.println("Enter a message to send to other users: ");
+
+			while (!this.closed()) {
+				readClientData();
+				sendData();
+			}
+
+			try {
+				listener.join();
+			} catch (InterruptedException ie) {
+				System.err.println(ie.getMessage());
+			}
+
+			this.outToServer.close();
+			this.inFromServer.close();
+			this.inFromStd.close();
+			socket.close();
+		} catch (BindException ex) {
+			System.err.println("Unable to bind a socket.");
+			ex.printStackTrace(System.err);
+			
+		} catch (ConnectException ex) {
+			System.err.println("Unable to connect to port.");
+			ex.printStackTrace(System.err);
+			
+		} catch (NoRouteToHostException ex) {
+			System.err.println("Routing Error.");
+			ex.printStackTrace(System.err);
+			
+		} catch (UnknownHostException ex) {
+			System.err.println("Unknown host.");
+			ex.printStackTrace(System.err);
+			
+		} catch (SocketException ex) {
+			System.err.println("Socket error");
+			ex.printStackTrace(System.err);
+			
+		} catch (IOException ex) {
+			System.err.println("IO Error");
+			ex.printStackTrace(System.err);
+		}
+
+}
 	
 	public void readClientData() throws FileNotFoundException, IOException {
 		System.out.println("Select Session Type");
-		System.out.println("0: List users");
-		System.out.println("1: Close Connection");
-		System.out.println("2: Send File");
-		System.out.println("3: Send Message");
+		System.out.println(ClypeData.USERLIST + ": List users");
+		System.out.println(ClypeData.LOGOUT + ": Close Connection");
+		System.out.println(ClypeData.FILE + ": Send File");
+		System.out.println(ClypeData.MESSAGE + ": Send Message");
 		
 		int session = inFromStd.nextInt();
 		
 		if (session == ClypeData.LOGOUT) {
-			closeConnection = true; 
+			closedConnection = true; 
 		}
 		else if (session == ClypeData.FILE) {
 			System.out.println("Enter filename");
@@ -152,7 +187,7 @@ public class ClypeClient {
 			}
 		}
 		else if (session == ClypeData.USERLIST) {
-			//TODO
+			this.dataToSendToServer = new MessageClypeData(userName, "list users", ClypeData.USERLIST);
 		}
 		
 		else { //message
@@ -199,7 +234,7 @@ public class ClypeClient {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + (closeConnection ? 1231 : 1237);
+		result = prime * result + (closedConnection ? 1231 : 1237);
 		result = prime
 				* result
 				+ ((dataToReceiveFromServer == null) ? 0
@@ -228,14 +263,16 @@ public class ClypeClient {
 		userName = this.userName == ((ClypeClient) other).getUserName();
 		hostName = this.hostName == ((ClypeClient) other).getHostName();
 		port = this.port == ((ClypeClient) other).getPort();
-		connection = this.closeConnection == ((ClypeClient) other).closeConnection;
+		connection = this.closedConnection == ((ClypeClient) other).closedConnection;
 		
 		data = this.dataToReceiveFromServer == ((ClypeClient) other).dataToReceiveFromServer && 
 				this.dataToSendToServer == ((ClypeClient) other).dataToSendToServer;
 		
 		return userName && hostName && port && connection && data;
-		
-		
+	}
+	
+	public boolean closed() {
+		return this.closedConnection;
 	}
 	
 	public String toString() {
@@ -245,13 +282,37 @@ public class ClypeClient {
 				this.dataToSendToServer + this.dataToReceiveFromServer;
 	}
 	
-	public static void main(String[] argv, int argc) {
-		switch(argc) {
+	public static void main(String[] args) {
+		ClypeClient client;
+		String username = DEFAULT_USER;
+		String hostname = DEFAULT_HOST;
+		int port = DEFAULT_PORT;
 		
-		
-		
+		if (args.length > 0) {
+			Scanner argReader = new Scanner(args[0]);
+			argReader.useDelimiter("[@:]");
+
+			if (argReader.hasNext()) {
+				username = argReader.next();
+			}
+			if (argReader.hasNext()) {
+				hostname = argReader.next();
+			}
+			if (argReader.hasNext()) {
+				port = argReader.nextInt();
+			}
+			else {
+				System.out.println("java -jar ClypeClient [username] [hostname] [port]");
+				System.exit(-1);
+			}
+			argReader.close();
 		}
+		
+		client = new ClypeClient(username, hostname, port);
+			
+		client.start();
 	}
+
 	
 
 }
--- src/main/ClypeServer.java
+++ src/main/ClypeServer.java
@@ -1,33 +1,40 @@
 package main;
 
-import data.ClypeData;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
 
+import data.ClypeData;
+import data.MessageClypeData;
 
 /**
  * @author pawlactb
- *
+ * 
  */
 public class ClypeServer {
 	private final static int DEFAULT_PORT = 7000;
-	
+	private final static int MIN_PORT = 1024;
+
 	private int port;
-	private boolean closeConnection;
-	private ClypeData dataToReceiveFromClient;
-	private ClypeData dataToSendToClient;
-	
-	
-	
+	private boolean closedConnection;
+
+	private ArrayList<ServerSideClientIO> serverSideClientIOList;
+
 	/**
-	 * @param port port number for incoming connections.
+	 * @param port port number to open for connections.
 	 */
-	public ClypeServer(int port) {
-		this.closeConnection = false;
+	public ClypeServer(int port) throws IllegalArgumentException {
+		this.closedConnection = false;
+		if(port < MIN_PORT){
+			throw new IllegalArgumentException();
+		}
 		this.port = port;
-		this.dataToReceiveFromClient = null;
-		this.dataToSendToClient = null;
+		serverSideClientIOList = new ArrayList<ServerSideClientIO>();
 	}
-	
-	
+
 	/**
 	 * Default port is 7000
 	 */
@@ -35,66 +42,145 @@ public class ClypeServer {
 		this(DEFAULT_PORT);
 	}
 	
+	public void start() {
+		try {
+			ServerSocket socket = new ServerSocket(port);
 	
-	public void start() {}
+			while (!this.closed()) {
+				Socket clientSocket = socket.accept();
+				ServerSideClientIO newServerSideClientIO = new ServerSideClientIO(this, clientSocket);
 	
-	/**
-	 * Receives data from the client...
-	 */
-	public void receiveData() {}
+				this.serverSideClientIOList.add(newServerSideClientIO);
+				Thread newClientThread = new Thread(newServerSideClientIO);
+				newClientThread.start();
+			}
 	
+			socket.close();
+	
+		} catch (IOException ioe) {
+			System.err.println("Issue with IO on server side");
+		}
+	}
+
+	public synchronized void broadcast(ClypeData dataToBroadcastToClient, ServerSideClientIO client) {
+		if (dataToBroadcastToClient.getType() == ClypeData.USERLIST) {
+			MessageClypeData usersData = new MessageClypeData(client.getUserName(), this.listUsers(),
+					ClypeData.USERLIST);
+			client.sendData(usersData);
+		} else
+			client.sendData();
+	}
+
+	public synchronized void broadcast(ClypeData dataToBroadcastToClients) {
+		if (dataToBroadcastToClients.getType() != ClypeData.USERLIST) {
+			for (ServerSideClientIO io : this.serverSideClientIOList) {
+				io.setSendDataToClient(dataToBroadcastToClients);
+				io.sendData();
+			}
+		} else {
+			for (ServerSideClientIO io : this.serverSideClientIOList) {
+				io.setSendDataToClient(dataToBroadcastToClients);
+				io.sendData();
+			}
+		}
+	}
+
+	public synchronized void remove(ServerSideClientIO serverSideClientToRemove) {
+		System.out.println("Closed Connection with: " + serverSideClientToRemove.getUserName() + " at " + new Date());
+		this.serverSideClientIOList.remove(serverSideClientToRemove);
+	}
+
+	public boolean closed() {
+		return this.closedConnection;
+	}
+
 	/**
 	 * Sends data to the client...
 	 */
-	public void sendData() {}
+	public void sendData() {
+	}
+
+	public String listUsers() {
+		String listOfUsers = "";
+		int userCounter = 1;
+		for (ServerSideClientIO io : this.serverSideClientIOList) {
+			listOfUsers += "User " + userCounter + ": " + io.getUserName() + "\n";
+			++userCounter;
+		}
+		return listOfUsers;
+}
+
 	
 	/**
 	 * Returns this object's port number.
+	 * 
 	 * @return
 	 */
-	public int getPort() { return this.port; }
+	public int getPort() {
+		return this.port;
+	}
+
+	public static void main(String[] args) {
+			try {
+				ClypeServer server;
+				if (args.length > 0) {
+					Scanner argScanner = new Scanner(args[0]);
+					int port = argScanner.nextInt();
+					argScanner.close();
+					server = new ClypeServer(port);
+				} else {
+					server = new ClypeServer();
+				}
 	
+				server.start();
 	
+			} catch (IllegalArgumentException iae) {
+				System.err.println(iae.getMessage());
+			}
+	
+	}
+
 	@Override
 	public int hashCode() {
-		int result = 17;
-		result = 37 * result + port;
-		if (dataToSendToClient != null)
-			result = 37 * result + dataToSendToClient.hashCode();
-		if (dataToReceiveFromClient != null)
-			result = 37 * result + dataToReceiveFromClient.hashCode();
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((serverSideClientIOList == null) ? 0 : serverSideClientIOList.hashCode());
+		result = prime * result + (closedConnection ? 1231 : 1237);
+		result = prime * result + port;
 		return result;
 	}
-	
-	/**
-	 * Compares two objects, ensures that they can both be cast to ClypeServer objects,
-	 * and returns whether the two objects contain all of the same data with the same values.
-	 */
+
 	@Override
-	public boolean equals(Object o) {
-		if (!(o instanceof ClypeServer))
-			return false;
-		ClypeServer cs = (ClypeServer) o;
-		
-		if (cs.port == this.port &&
-				cs.closeConnection == this.closeConnection &&
-				cs.dataToReceiveFromClient.equals(this.dataToReceiveFromClient) &&
-				cs.dataToSendToClient.equals(this.dataToSendToClient))
+	public boolean equals(Object obj) {
+		if (this == obj)
 			return true;
-		return false;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof ClypeServer))
+			return false;
+		ClypeServer other = (ClypeServer) obj;
+		if (serverSideClientIOList == null) {
+			if (other.serverSideClientIOList != null)
+				return false;
+		} else if (!serverSideClientIOList.equals(other.serverSideClientIOList))
+			return false;
+		if (closedConnection != other.closedConnection)
+			return false;
+		if (port != other.port)
+			return false;
+		return true;
 	}
-	
-	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
+
 	@Override
 	public String toString() {
-		return "ClypeClient.\n" +
-				"Data to send to client: " + dataToSendToClient.toString() + "\n" +
-				"Data to receive from client: " + dataToReceiveFromClient.toString() + "\n" +
-				"Connection: " + this.closeConnection + "\n" +
-				"Port: " + this.port + "\n";
-				
+		final int maxLen = 10;
+		return "ClypeServer [port="
+				+ port
+				+ ", closedConnection="
+				+ closedConnection
+				+ ", clientIOList="
+				+ (serverSideClientIOList != null ? serverSideClientIOList.subList(0,
+						Math.min(serverSideClientIOList.size(), maxLen)) : null) + "]";
 	}
 }
